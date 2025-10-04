8e5d6a82c6564a9de2faf49adfc7c1eaf256a1b6
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2025-10-04 16:22:59 +0530

    Added a  GET endpoint to view a note by ID

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ def add_note(note: str):
 @app.get("/notes")
 def view_notes():
     return notes
+
+@app.get("/notes/{note_id}")
+def view_note(note_id: int):
+    return {f"{note_id}":notes[note_id]}
+
