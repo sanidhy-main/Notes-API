e1da5bb2995f037276fa7bcc27528b0270b11e6d
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2025-10-04 16:33:40 +0530

    Update Notes API: separate title and content, improve ID/counter system

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-notes = {}
+notes_with_id = {}
 
 counter = 0
 @app.post("/note")
-def add_note(note: str):
+def add_note(title: str, content: str):
     global counter
-    notes[counter+1] = note
+    note = {"Title": title, "Content": content}
+    notes_with_id[counter] = note
     counter += 1
-    return {"Note": note}
+    return {counter-1: {title: content}}
 
 @app.get("/notes")
 def view_notes():
-    return notes
+    return notes_with_id
 
 @app.get("/notes/{note_id}")
 def view_note(note_id: int):
-    return {f"{note_id}":notes[note_id]}
+    return f"{note_id}: {notes_with_id[note_id]}"
 
