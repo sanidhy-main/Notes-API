8fc2279e1a7ad452c1745d917584c8833959731a
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-01-03 13:37:42 +0400

    fixed some small stuff that broke after deleting HTML files

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
-from fastapi import HTTPException
+from fastapi.responses import RedirectResponse
+
 app = FastAPI()
 notes_with_id = {}
 counter = 1
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
 
 @app.post("/note")
 def add_note(title: str, content: str):
     global counter
     note = {"Title": title, "Content": content}
-    if not title or not content:
-        raise HTTPException(status_code=404, detail="Title or Content cannot be empty")
     notes_with_id[counter] = note
     counter += 1
     return {counter-1: {title: content}}
@@ -20,23 +23,15 @@ def view_notes():
 
 @app.get("/notes/{note_id}")
 def view_note(note_id: int):
-    if note_id not in notes_with_id:
-        raise HTTPException(status_code=404, detail="Note not found")
-    else: return {note_id: notes_with_id[note_id]}
+    return {note_id: notes_with_id[note_id]}
 
 @app.delete("/notes/{note_id}")
 def delete_note(note_id: int):
-    if note_id not in notes_with_id:
-        raise HTTPException(status_code=404, detail="Note not found")
     del notes_with_id[note_id]
     return {"success": True, "message": "Note deleted"}
 
 @app.put("/notes/{note_id}")
 def update_note(note_id: int, title: str, content: str):
-    if not title or not content:
-        raise HTTPException(status_code=404, detail="Title or Content cannot be empty")
-    if note_id not in notes_with_id:
-        raise HTTPException(status_code=404, detail="Note not found")
     note = notes_with_id[note_id]
     note["Title"] = title
     note["Content"] = content
